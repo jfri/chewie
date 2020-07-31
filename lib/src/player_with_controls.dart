d53a15e287492624de1f81d6a20242961e66e3e3
Jonathan Friesen <jonathanfriesen@gmail.com>
2020-07-31 16:56:26 -0700

    Allow controls and player to have separate aspect ratios

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -17,25 +17,19 @@ class PlayerWithControls extends StatelessWidget {
     return Center(
       child: Container(
         width: MediaQuery.of(context).size.width,
-        child: AspectRatio(
-          aspectRatio:
-              chewieController.aspectRatio ?? _calculateAspectRatio(context),
-          child: _buildPlayerWithControls(chewieController, context),
-        ),
+        child: _buildPlayerWithControls(chewieController, context),
       ),
     );
   }
 
-  Container _buildPlayerWithControls(
-      ChewieController chewieController, BuildContext context) {
+  Container _buildPlayerWithControls(ChewieController chewieController, BuildContext context) {
     return Container(
       child: Stack(
         children: <Widget>[
           chewieController.placeholder ?? Container(),
           Center(
             child: AspectRatio(
-              aspectRatio: chewieController.aspectRatio ??
-                  _calculateAspectRatio(context),
+              aspectRatio: chewieController.aspectRatio ?? _calculateAspectRatio(context),
               child: VideoPlayer(chewieController.videoPlayerController),
             ),
           ),
